a1cbb8a9734c0cad7e9f6e20442a89d9a5ee8614
Jonathan Ho <johnnyho82@gmail.com>
2020-01-23 00:08:39 -0500

    Refactor eating cookies code to use list as cache rather than dictionary

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,13 +7,24 @@ import sys
 # recursive solution
 
 
-def eating_cookies(n, list=None, cache=None):
+# def eating_cookies(n, cache=None):
+#     if cache is None:
+#         cache = {0: 1, 1: 1, 2: 2}
+#     if n not in cache:
+#         cache[n] = eating_cookies(n - 3, cache) \
+#             + eating_cookies(n - 2, cache) \
+#             + eating_cookies(n - 1, cache)
+#     return cache[n]
+
+def eating_cookies(n, cache=None):
     if cache is None:
-        cache = {0: 1, 1: 1, 2: 2}
-    if n not in cache:
-        cache[n] = eating_cookies(n - 3, list, cache) \
-            + eating_cookies(n - 2, list, cache) \
-            + eating_cookies(n - 1, list, cache)
+        cache = [1, 1, 2] + [0 for i in range(n - 2)]
+    elif cache:
+        cache[0], cache[1], cache[2] = 1, 1, 2
+    if cache[n] == 0:
+        cache[n] = eating_cookies(n - 3, cache) \
+            + eating_cookies(n - 2, cache) \
+            + eating_cookies(n - 1, cache)
     return cache[n]
 
 
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,21 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    plays = ['rock', 'paper', 'scissors']
+    output_list = []
+    # output_list = [0] * (3 ** n)
+
+    if n == 0:
+        output_list.append([])
+
+    return output_list
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
