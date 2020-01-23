0162a2df41a79cbb3610d1d7a6c0cc945dd13e34
Jonathan Ho <johnnyho82@gmail.com>
2020-01-23 16:32:54 -0500

    Finish and test rock paper scissors problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -7,80 +7,15 @@ def rock_paper_scissors(n):
     output = []
     plays = ['rock', 'paper', 'scissors']
 
-    # if n == 0:
-    #     output_list.append([])
-    # if n == 1:
-    #     for play in plays:
-    #         output_list.append([play])
-    # if n == 2:
-    #     for a in plays:
-    #         for b in plays:
-    #             output_list.append([a, b])
-    # if n == 3:
-    #     for a in plays:
-    #         for b in plays:
-    #             for c in plays:
-    #                 output_list.append([a, b, c])
-    # if n == 4:
-    #     for a in plays:
-    #         for b in plays:
-    #             for c in plays:
-    #                 for d in plays:
-    #                     output_list([a, b, c, d])
-
-    # def recur(n, round=[]):
-    #     if n == 0:
-    #         output.append(round)
-    #         return output
-    # for i in range(len(plays)):
-    #     round = []
-    #     print('A', round)
-    #     print('B', plays[i])
-    #     round.append(plays[i])
-    #     print('C', round)
-    #     recur(n - 1, round)
-    # if n == 1:
-    #     round = []
-    #     round.append('rock')
-    #     recur(n - 1, round)
-    #     round = []
-    #     round.append('paper')
-    #     recur(n - 1, round)
-    #     round = []
-    #     round.append('scissors')
-    #     recur(n - 1, round)
-    # if n == 2:
-    #     round = []
-    #     round.append('rock')
-    #     round.append('rock')
-    #     recur(0, round)
-    #     round = []
-    #     round.append('rock')
-    #     round.append('paper')
-    #     recur(0, round)
-    #     round = []
-    #     round.append('rock')
-    #     round.append('scissors')
-    #     recur(0, round)
-    # else:
-    #     round = []
-
-    def recur(n, round=[]):
+    def build_moves(n, moves):
         if n == 0:
-            print('2')
-            # output.append(round)
-            return
-        for play in plays:
-            round = []
-            print('1', play)
-            round.append(play)
-            recur(n - 1, round)
-            output.append(round)
-            # recur(n - 1, round)
+            output.append(moves)
+        else:
+            for play in plays:
+                build_moves(n - 1, moves + [play])
+        return output
 
-    recur(n, [])
-
-    return output
+    return build_moves(n, [])
 
 
 if __name__ == "__main__":
