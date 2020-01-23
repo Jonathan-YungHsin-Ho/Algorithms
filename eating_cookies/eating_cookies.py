c67703f47858a7ecc324d3cc8dceeecabd04a318
Jonathan Ho <johnnyho82@gmail.com>
2020-01-23 13:01:59 -0500

    Use dictionary cache for eating cookies problem

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,28 +7,28 @@ import sys
 # recursive solution
 
 
-# def eating_cookies(n, cache=None):
-#     if cache is None:
-#         cache = {0: 1, 1: 1, 2: 2}
-#     if n not in cache:
-#         cache[n] = eating_cookies(n - 3, cache) \
-#             + eating_cookies(n - 2, cache) \
-#             + eating_cookies(n - 1, cache)
-#     return cache[n]
-
 def eating_cookies(n, cache=None):
-    if not cache:
-        cache = [1, 1, 2] + [0 for i in range(n - 2)]
-    else:
-        cache[0], cache[1], cache[2] = 1, 1, 2
-
-    if cache[n]:
-        return cache[n]
-    else:
+    if cache is None or type(cache) == list:
+        cache = {0: 1, 1: 1, 2: 2}
+    if n not in cache:
         cache[n] = eating_cookies(n - 3, cache) \
             + eating_cookies(n - 2, cache) \
             + eating_cookies(n - 1, cache)
-        return cache[n]
+    return cache[n]
+
+# def eating_cookies(n, cache=None):
+#     if not cache:
+#         cache = [1, 1, 2] + [0 for i in range(n - 2)]
+#     else:
+#         cache[0], cache[1], cache[2] = 1, 1, 2
+
+#     if cache[n]:
+#         return cache[n]
+#     else:
+#         cache[n] = eating_cookies(n - 3, cache) \
+#             + eating_cookies(n - 2, cache) \
+#             + eating_cookies(n - 1, cache)
+#         return cache[n]
 
 
 if __name__ == "__main__":
