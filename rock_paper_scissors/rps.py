22ce3e81e9895b94e061dc6d156514c21302c9db
Jonathan Ho <johnnyho82@gmail.com>
2020-01-24 10:12:19 -0500

    Rename variables in rock paper scissors for readability

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -7,12 +7,12 @@ def rock_paper_scissors(n):
     output = []
     plays = ['rock', 'paper', 'scissors']
 
-    def build_moves(n, moves=[]):
-        if n == 0:
+    def build_moves(moves_remaining, moves=[]):
+        if moves_remaining == 0:
             output.append(moves)
         else:
             for play in plays:
-                build_moves(n - 1, moves + [play])
+                build_moves(moves_remaining - 1, moves + [play])
         return output
 
     return build_moves(n)
